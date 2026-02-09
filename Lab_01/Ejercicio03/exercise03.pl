86fc8144301eaf0fde8a9baf80f7526dba724fc3
levik354 <nicole.centanaro-e@mail.escuelaing.edu.co>
2026-02-09 08:31:32 -0500

    Update exercise03.pl

--- Lab_01/Ejercicio03/exercise03.pl
+++ Lab_01/Ejercicio03/exercise03.pl
@@ -1,1 +1,30 @@
+% Hechos: estudiante y su nota final en un curso
+nota(ana, 4.5).
+nota(luis, 2.8).
+nota(maria, 3.7).
+nota(juan, 5.0).
+nota(pedro, 2.3).
+
+
+%No aprobado
+reprueba(X,reprueba):- nota(X,Y), Y<3.0.
+
+%Aprobado
+aprueba(X,aprobado):- nota(X,Y), Y>2.9.
+
+%Rango
+rango(X,Min,Max):- nota(X,Y), X=X, Y>Min, Y<Max.
+
+%Clasificacion
+clasificacion_nota(Nota,Tipo):- Nota<3.0, Tipo = reprueba.
+clasificacion_nota(Nota,Tipo):- Nota>2.9, Nota<4.0, Tipo = aprobado.
+clasificacion_nota(Nota,Tipo):- Nota>3.9, Nota<4.5, Tipo = notable.
+clasificacion_nota(Nota,Tipo):- Nota>4.4, Nota<5.1, Tipo = excellente.
+
+
+%Distancia ecludiea
+distancia((X1,Y1),(X2,Y2),D):- D is sqrt((X2-X1)**2 + (Y2-Y1)**2).
+
+
+
 
