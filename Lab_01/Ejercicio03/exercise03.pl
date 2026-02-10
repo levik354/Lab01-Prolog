d8c7418952447699d09b8c44896c529f61424fea
levik354 <nicole.centanaro-e@mail.escuelaing.edu.co>
2026-02-10 07:19:14 -0500

    Update exercise03.pl

--- Lab_01/Ejercicio03/exercise03.pl
+++ Lab_01/Ejercicio03/exercise03.pl
@@ -25,6 +25,17 @@ clasificacion_nota(Nota,Tipo):- Nota>4.4, Nota<5.1, Tipo = excellente.
 %Distancia ecludiea
 distancia((X1,Y1),(X2,Y2),D):- D is sqrt((X2-X1)**2 + (Y2-Y1)**2).
 
+% Caso base: lista vacía
+distancia_total([], 0).
+
+% Caso base: un solo punto
+distancia_total([_], 0).
+
+% Caso recursivo: dos o más puntos
+distancia_total([P1, P2 | Resto], Total) :-
+    distancia(P1, P2, D),
+    distancia_total([P2 | Resto], Subtotal),
+    Total is D + Subtotal.
 
 
 
