d4d349300d0bcb3791625f1b9b8ff3a0bbf6b837
levik354 <nicole.centanaro-e@mail.escuelaing.edu.co>
2026-02-15 12:56:59 -0500

    Update Exercise_06.pl

--- Lab_01/Ejercicio 06/Exercise_06.pl
+++ Lab_01/Ejercicio 06/Exercise_06.pl
@@ -1,4 +1,5 @@
 
+
 %Escribe una regla que verifique si un elemento es miembro de una lista. 
 miembro(X,[X|_]).
 miembro(X,[_|T]) :-
@@ -19,8 +20,31 @@ proper_length(Lista, Longitud) :-
 
 
 %Escribe una regla "concatena" que dadas dos listas, las concatene en otra.
-dos_listas([X1|Tail1],[X2|Tail2]):-
-    new_caracteres(X1,X2),
-    new_lista([_|X1,X2],
-    dos_listas(Tail1,Tail2).
-              
+concatena([], L, L).
+concatena([X|Xs], L2, [X|R]) :-
+	concatena(Xs, L2, R).
+
+
+%Escribe una regla que permite obtener en una lista los N elementos de las serie de fibonacci, 
+%usa la regla predifina append(L1,L2,L3), que concatena L1 y L2, en L3.
+fibonacci(N, L) :-
+    N > 0,
+    fib_aux(N, [0,1], L).
+fib_aux(1, [F|_], [F]).
+fib_aux(2, [F1,F2|_], [F1,F2]).
+fib_aux(N, [F1,F2|_], R) :-
+    N > 2,
+    F3 is F1 + F2,
+    N1 is N - 1,
+    append([F1], R1, R),
+    fib_aux(N1, [F2,F3], R1).
+
+%Escribe una regla que determine el reverso de una lista.
+reverso([], []).
+reverso([X|Xs], R) :-
+    reverso(Xs, R1),
+    append(R1, [X], R).
+
+%Escribe una regla que determine si una lista es palindroma.
+palindroma(L) :-
+    reverso(L, L).
